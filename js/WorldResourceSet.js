4921750fd6dc935a6ff9802147046d418b1aa280
Thomas Leing <bluezebragames@gmail.com>
2019-08-15 10:28:36 -0700

    WRS saving and loading

--- js/WorldResourceSet.js
+++ js/WorldResourceSet.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { createResource } from "./Resource.js";
+import { createResource, loadResource } from "./Resource.js";
 
 class WorldResourceSet {
     constructor (resourceNames, resourceDiv, world) {
@@ -34,17 +34,12 @@ class WorldResourceSet {
     }
 
     save () {
-        let saveComponents = [];
+        let save = {};
+        save.f = this.focusedResource ? this.focusedResource.internalName : undefined;
         for (const resource of this.resourceList) {
             saveComponents.push(resource.save());
         }
         return saveComponents;
-    }
-
-    load (saveComponents) {
-        for (let i = 0; i < this.resourceList.length; ++i) {
-            this.resourceList[i].load(saveComponents[i]);
-        }
     }
 
     clear () {
@@ -73,4 +68,27 @@ class WorldResourceSet {
     // TODO: idk where this should go, but should do something when there are no upgrades to buy -- right now it looks weird if there's nothing to buy (c.f. cookie clicker?)
 }
 
-export { WorldResourceSet };
+function createWorldResourceSet (resourceNames, resourceDiv, world) {
+    const resourceList = [];
+
+    for (const resourceName of resourceNames) {
+        // TODO: consider whether it's a good idea to have both a list and a map of the same thing
+        resourceList.push(createResource(resourceName, resourceDiv, world));
+    }
+
+    return new WorldResourceSet(resourceList, resourceDiv, world);
+}
+
+function loadWorldResourceSet (save, resourceDiv, world) {
+    const resourceList = [];
+    for (const resourceSave of save.r) {
+        resourceList.push(loadResource(resourceSave, resourceDiv, world));
+    }
+
+    const wrs = new WorldResourceSet(resourceList, resourceDiv, world);
+    wrs.setFocusedResource(wrs[save.f]);
+
+    return wrs;
+}
+
+export { createWorldResourceSet, loadWorldResourceSet };
